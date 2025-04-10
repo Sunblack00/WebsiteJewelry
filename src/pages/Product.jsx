827ba9953c2fec5f886a0920a6169e73210873a7
HuuThai <levanhuuthai2004@gmail.com>
2025-04-10 16:43:04 +0700

    Lam ProductDetail

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,15 @@
-import React from "react";
+// import React from "react";
 
+// export default function Product() {
+//   return <div></div>;
+// }
+
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Product() {
-  return <div></div>;
+    return (
+        <Link to={`/product/1`}>
+            <div>Alo</div>
+        </Link>
+    );
 }
