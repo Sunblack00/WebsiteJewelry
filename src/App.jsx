cd2f9fb77e6332dd117f18ed22fc52fcbfc8014b
HuuThai <levanhuuthai2004@gmail.com>
2025-04-10 15:49:19 +0700

    Tao du lieu

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,22 @@ import Contact from "./pages/Contact";
 import Product from "./pages/Product";
 import Blog from "./pages/Blog";
 import Account from "./components/Account";
+import ProductDetail from "./pages/ProductDetail";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Banner></Banner>}></Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Banner></Banner>}></Route>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/product" element={<Product />} />
+                <Route path="/account" element={<Account />} />
+                <Route path="/product/:id" element={<ProductDetail />} />
+            </Routes>
+        </>
+    );
 }
