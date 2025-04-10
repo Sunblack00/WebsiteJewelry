48856364dc374a52fc602b07d02e40b78e5e9901
Hoang Sang <93880956+Sunblack00@users.noreply.github.com>
2025-04-10 17:23:09 +0700

    update hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Banner from "./components/home/Banner";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -11,19 +10,18 @@ import Account from "./components/Account";
 import ProductDetail from "./pages/ProductDetail";
 
 export default function App() {
-    return (
-        <>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Banner></Banner>}></Route>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/product" element={<Product />} />
-                <Route path="/account" element={<Account />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+      </Routes>
+    </>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/home/Banner";
+import Hero from "../components/home/Hero";
 
 export default function Home() {
   return (
     <>
       <Banner></Banner>
+      <Hero></Hero>
     </>
   );
 }
